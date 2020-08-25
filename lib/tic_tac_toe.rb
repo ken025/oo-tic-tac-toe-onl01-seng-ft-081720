9b0a1ebb53f484d789bac315dd8b59d9efe4ea41
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-25 05:27:09 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,5 +60,23 @@ end
   end
 end
 
-def won? 
+def won?
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+
+    position_1 = @board[index_0]
+    position_2 = @board[index_1]
+    position_3 = @board[index_2]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+       win_combo
+       
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+       win_combo
+    end
+  }
+   false
+end
 end 
