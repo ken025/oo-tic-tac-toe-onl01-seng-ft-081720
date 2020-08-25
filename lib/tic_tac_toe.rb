8decb6dcf3b07f32fc8fcb878b5aeab6294ede1e
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-25 05:38:04 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -90,15 +90,16 @@ end
      end 
    end 
    
-    def play
-    @board = Array.new(9, " ")
-    turn until over?
-  
-    won? ? puts("Congratulations #{winner}!") : puts("Cat's Game!")
-
-    puts "Would you like to play again? (Y or N)"
-    
-    gets.strip.downcase == "y" || gets.strip.downcase == "yes" ? play : puts("Goodbye!")
+  def play
+  until over? == true
+    turn
   end
+
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
 
 end 
